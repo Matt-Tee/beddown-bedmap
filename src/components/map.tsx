0a7f401dc64c9f16538a6c7fe8f32926647c5956
Manik747 <m.martin747@gmail.com>
2019-08-24 15:01:00 +1000

    map screen uploaded

---
+++ src/components/map.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactMapboxGl from 'react-mapbox-gl';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+
+const MapArea = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g'
+});
+
+export default function Map() {
+
+  return (
+    <div>
+      <MapArea
+        style='mapbox://styles/mapbox/streets-v9' // eslint-disable-line
+        containerStyle={{
+          height: '600px',
+          width: '100vw'
+        }}
+        center={[153.0251, -27.4698]}
+      ></MapArea>
+    </div>
+  );
+}
+
+ReactDOM.render(<Map />, document.getElementById('root'));
