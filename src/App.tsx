90d7b94d77cc45780c084b28312b93f4560f39cb
Manik747 <m.martin747@gmail.com>
2019-08-25 13:39:31 +1000

    header complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
-import Map from './components/map'
+import Map from './components/map';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  background: #19a0fb;
+  width: 100%;
+`;
+const HeaderText = styled.div`
+  color: white;
+  font-family: Roboto;
+  padding: 24px;
+  font-size: 24px;
+  text-align: center;
+`;
 
 const App: React.FC = () => {
-  return <div>
-    <Map />
-  </div>;
+  return (
+    <div>
+    <Header>
+      <HeaderText> BedDown</HeaderText>
+    </Header>
+      <Map />
+    </div>
+  );
 };
 
 export default App;
