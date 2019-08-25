cf98a509cfa79bfe3744cfff375a36a414d0ef18
Matthew <matthew.leacy@live.com.au>
2019-08-25 13:15:00 +1000

    booking form

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Booking(props:any) {
 
-    function renderBookingForm(event) {
-        
+    const [ displayInitiated, displayInitiatedSetter] = useState(false);
+    const [ name, setName] = useState('')
+    const [ phone, setPhone] = useState('')
+
+    function renderBookingForm(event: React.MouseEvent<HTMLButtonElement,MouseEvent>) {
+        event.preventDefault();
+        displayInitiatedSetter(true);
     }
+
+    function changeHandler(event: React.ChangeEvent<HTMLInputElement>){
+        if (event.target.name === "name"){
+            setName(event.target.value)
+        }else if (event.target.name === "phone"){
+            setPhone(event.target.value)
+        }
+    }
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+    }
+
+    const initiateBookingButton = <button onClick={renderBookingForm}>BOOK A BED</button> ;
+
+    const bookingForm = (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <label> Name:
+            <input name="name" type="text" value={name} onChange={changeHandler} />
+            </label>
+            <label> Mobile Number:
+            <input name="phone" type="text" value={phone} onChange={changeHandler} />
+            </label>
+            <button type="submit">Confirm Booking</button>
+        </form>
+    </div>)
   
     return (
         <div>
             <h1>{props.provider.name}</h1>
             <p>{props.location.address}</p>
-            <button onClick={renderBookingForm}>BOOK A BED</button>
+            
+            {displayInitiated ? initiateBookingButton: bookingForm }
+
         </div>
     );
 }
